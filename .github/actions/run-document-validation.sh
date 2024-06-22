42fd8279a0e21311eebfa6747a4579418f1fb036
withanage <dulip.withanage@gmail.com>
2024-06-22 22:36:55 +0200

    document validation set failure on first error

--- .github/actions/run-document-validation.sh
+++ .github/actions/run-document-validation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set +e
+set -e # Fail on first error
 # Look for help filenames referred to in templates and check that they all exist (in English)
 ERRORS=0
 for filename in `find . -name \*.tpl -exec sed -n -e "s/.*{help[^}]file=\"\([^\"#]\+\)[#\"].*/\1/p" "{}" ";"`; do
@@ -10,11 +10,10 @@ for filename in `find . -name \*.tpl -exec sed -n -e "s/.*{help[^}]file=\"\([^\"
   fi
 done
 if [ $ERRORS -ne 0 ]; then
-  exit 0
+  exit -1
 fi
 # Generate a quick report of the differences between the files listed in templates and the available files.
 find . -name \*.tpl -exec sed -n -e "s/.*{help[^}]file=\"\([^\"]\+\)\".*/\1/p" "{}" ";" | sort | uniq > /tmp/template-help-references.txt
 cat docs/manual/en/SUMMARY.md | sed -n -e "s/.*(\([^)]\+\))/\1/p" | sort | uniq > /tmp/help-files.txt
 echo "Unreferenced help files:"
 diff /tmp/template-help-references.txt /tmp/help-files.txt | grep -e "^>"
-set -e
