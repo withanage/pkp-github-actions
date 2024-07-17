b0c6388d2c73b3a87c75d61cf152ca3ad44e18ab
withanage <dulip.withanage@gmail.com>
2024-07-17 23:31:39 +0200

    Delete plugin folder for plugin actions

--- .github/actions/run-plugin-actions.sh
+++ .github/actions/run-plugin-actions.sh
@@ -8,7 +8,15 @@ git clone -b "${GITHUB_EVENT_HEAD_REF}" "${GITHUB_EVENT_HEAD_REPO_HTML_URL}" "${
 cd "${GITHUB_EVENT_HEAD_REPO_NAME}"
 pluginType=$(grep '<type>' version.xml | sed -n 's/.*<type>\(.*\)<\/type>.*/\1/p')
 pluginPath=$(echo "$pluginType" | tr '.' '/')
-mv ~/"${GITHUB_EVENT_HEAD_REPO_NAME}"  ~/"${APPLICATION}"/"$pluginPath"
+
+PLUGIN_DIR=~/"${APPLICATION}"/"${pluginPath}"
+
+if [ -d "$PLUGIN_DIR" ]; then
+  rm -rf "$PLUGIN_DIR"
+fi
+
+mv ~/"${GITHUB_EVENT_HEAD_REPO_NAME}"  "$PLUGIN_DIR"
+
 cd ~/"${APPLICATION}"
 if [ -e "${pluginPath}/${GITHUB_EVENT_HEAD_REPO_NAME}/.github/actions/tests.sh" ]; then
   source "${pluginPath}/${GITHUB_EVENT_HEAD_REPO_NAME}/.github/actions/tests.sh"
