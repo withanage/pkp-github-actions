05f27d62748843c5413cdac9c644179dd9cf49cb
withanage <dulip.withanage@gmail.com>
2024-12-16 12:48:11 +0100

    Custom python2 isntallation

--- .github/actions/install-validation-tools.sh
+++ .github/actions/install-validation-tools.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -e
+wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
+sudo tar xzf Python-2.7.9.tgz
+cd Python-2.7.9
+sudo ./configure --enable-optimizations
+sudo make altinstall
 wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
 sudo python2 get-pip.py
 sudo pip install six
